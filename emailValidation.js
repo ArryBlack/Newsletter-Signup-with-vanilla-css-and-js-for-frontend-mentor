fa377d114fdc82b9ad9c56b7bd3ad647eb140ab4
ArryBlack <sikdararyarishi.india@gmail.com>
2024-05-03 00:56:23 +0530

    email validation

--- emailValidation.js
+++ emailValidation.js
@@ -1,16 +1,30 @@
+
 const email = document.getElementById('email');
 const submitBtn = document.querySelector('.submit_btn');
 const errorMessage = document.querySelector('.error_message');
 const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-submitBtn.addEventListener('click', (e) => {
-    if (!email.value.match(mailformat)) {
-        errorMessage.style.display = 'block';
-        email.classList.add('error');
-        e.preventDefault();
+const display= document.querySelector('.email_display');
+
+   
+
+if(submitBtn){
+    submitBtn.addEventListener('click', (e) => {
+        if (!email.value.match(mailformat)) {
+            errorMessage.style.display = 'block';
+            email.classList.add('error');
+            e.preventDefault();
+           
+        } else {
+            errorMessage.style.display = 'none';
+            email.classList.remove('error');
+            
+          
+        
+        }
+    
        
-    } else {
-        errorMessage.style.display = 'none';
-        email.classList.remove('error');
-       
-    }
-});
+        
+    });
+}
+
+
